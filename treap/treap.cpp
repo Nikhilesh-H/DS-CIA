09e5f0bce6d546c0f5ac4a3c856c426c8c166951
Nikhilesh-H <97732838+Nikhilesh-H@users.noreply.github.com>
2024-04-27 11:38:01 +0530

    Create treap.cpp

---
+++ treap/treap.cpp
@@ -0,0 +1,60 @@
+int main(){
+    int choice;
+    int key;
+    int data;
+    treap t;
+    while(1){
+        printf("\nMenu:\n\t1. Insert\n\t2. Delete\n\t3. Search\n\t4. Display\n\t5. Exit\nEnter you choice : ");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1:{
+                printf("Enter key : ");
+                scanf("%d",&key);
+                printf("Enter data : ");
+                scanf("%d",&data);
+                if(t.insert(key, data)){
+                    printf("Successfully inserted (%d,%d).\n",key,data);
+                }
+                else{
+                    printf("Insertion unsuccessfull.\n");
+                }
+                break;
+            }
+            case 2:{
+                printf("Enter key : ");
+                scanf("%d",&key);
+                if(t.search(key, t.getroot())){
+                    t.del(key,t.getroot());
+                    printf("Successfully deleted %d\n", key);
+                }
+                else{
+                    printf("%d - Not found.\n", key);
+                }
+                break;
+            }
+            case 3:{
+                printf("Enter key : ");
+                scanf("%d",&key);
+                if(t.search(key, t.getroot())){
+                    printf("%d - Found.\n",key);
+                }
+                else{
+                    printf("%d - Not found.\n", key);
+                }
+                break;
+            }
+            case 4:{
+                t.display(t.getroot());
+                break;
+            }
+            case 5:{
+                t.~treap();
+                exit(0);
+            }
+            default:{
+                printf("Error! Enter a valid choice.\n");
+            }
+        }
+    }
+    return 0;
+}
